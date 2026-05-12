c7b9151713cdb207a9a535383c02ecba36915a25
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-05-13 03:11:06 +0530

    Add files via upload

---
+++ abstract_demo.java
@@ -0,0 +1,22 @@
+abstract class rushi
+{
+    public int i, j;
+
+    public int Add(int a , int b)   // Concrete method
+    {
+        return a + b;
+    }
+
+    
+}
+
+
+
+class abstract_demo 
+{
+    public static void main (String A[])
+        {
+            Demo dobj = new demo ();
+        }
+
+}
